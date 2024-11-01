178a776bc70a74e88d5670268c0d569c1e8f5efd
muffin819 <ch.engleongkai0310@gmail.com>
2024-11-01 00:33:46 -0700

    initial commit

---
+++ constants/index.ts
@@ -0,0 +1,5 @@
+export const RPC_ENDPOINT="https://mainnet.helius-rpc.com/?api-key=80e81ffd-0711-42b9-afac-e64d099b279e"
+export const RPC_WEBSOCKET_ENDPOINT='wss://atlas-mainnet.helius-rpc.com/?api-key=6df11b91-2dd6-4564-8ff6-f3564842048d'
+export const JUP_AGGREGATOR=process.env.JUP_AGGREGATOR;
+export const TARGET_WALLET=process.env.TARGET_WALLET
+export const MAXIMUM_BUY_AMOUNT=process.env.MAXIMUM_BUY_AMOUNT
